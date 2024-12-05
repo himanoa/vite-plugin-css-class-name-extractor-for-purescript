f1e379b6b956c939107d6da05f0372bda04d7959
himanoa <matsunoappy@gmail.com>
2024-12-06 01:05:54 +0900

    Implemented plugin entrypoint

---
+++ index.js
@@ -0,0 +1,21 @@
+import { handleWatch } from './output/VitePluginClassNameExtractor.Watch/index.js'
+
+export function vitePluginCssClassNameExtractorForPureScript(config) {
+  let server;
+
+  return {
+    name: "vite-plugin-css-class-name-extractor-for-purescript",
+    apply: 'serve',
+    configureServer(s) {
+      server = s
+    },
+    buildStart() {
+      const events = ["add", "addDir", "change", "unlink", "unlinkDir"]
+      for (const e of events) {
+        server.watcher.on(e, (entry) => {
+          handleWatch(entry)
+        })
+      }
+    }
+  }
+}
