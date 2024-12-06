fd97f2cae5bb61acaf80d0a1b3b815047a4f780e
himanoa <matsunoappy@gmail.com>
2024-12-06 19:59:55 +0900

    Add example

--- example/src/components/Colors.module.purs
+++ example/src/components/Colors.module.purs
@@ -1,4 +1,8 @@
-module Colors.Styles (wrapper) where
+module Colors.Styles (wrapper,foo,bar) where
 foreign import _styles :: String -> String
 wrapper :: String
 wrapper = _styles "wrapper"
+foo :: String
+foo = _styles "foo"
+bar :: String
+bar = _styles "bar"
